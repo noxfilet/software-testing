c9ef0c2903dfda5b684a013256df8adcb694df7d
Thanh Pham <phamvuvanthanh2407@gmail.com>
2025-11-20 16:39:07 +0200

    limit test files to 10 selected files

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,10 +2,24 @@ module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/tests/**/*.test.js'],
   collectCoverage: true,
-  collectCoverageFrom: [
-    'src/**/*.js',
-    '!src/**/.internal/**'
-  ],
+  // collectCoverageFrom: [
+  //   'src/**/*.js',
+  //   '!src/**/.internal/**'
+  // ],
+
+   collectCoverageFrom: [
+        'src/add.js',
+        'src/capitalize.js',  
+        'src/countBy.js',  
+        'src/drop.js', 
+        'src/eq.js', 
+        'src/filter.js',      
+        'src/get.js',
+        'src/isEmpty.js',
+        'src/keys.js',  
+        'src/words.js',
+    ],
+
   // We don't need Babel transforms; this keeps it simple
   transform: {}
 };
